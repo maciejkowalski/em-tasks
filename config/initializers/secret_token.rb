5eac419af5f0cbdef40fa81b03cbd1817a652c42
Maciej Hitsu Kowalski <maciej33kowalski@gmail.com>
2014-05-02 16:34:11 +0200

    git add all

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EmTasks::Application.config.secret_key_base = '06712cf1589b97931c6372113b314be85c014ce9005c9800a1978684d0289c4fe95e36b212b2eb0a72a38ac869de22df89631751657cc83dbeeede676a4e9c0c'
