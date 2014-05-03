df67054c47dfbdee8f8c709f128a631fbc567e71
Maciej Hitsu Kowalski <maciej33kowalski@gmail.com>
2014-05-03 13:09:34 +0200

    implement List controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,29 @@
 class ListsController < ApplicationController
+  respond_to :json
+
+  def index
+    respond_with List.all
+  end
+
+  def show
+    respond_with List.find(params[:id])
+  end
+
+  def create
+    respond_with List.create(list_params)
+  end
+
+  def update
+    respond_with List.update(params[:id], list_params)
+  end
+
+  def destroy
+    respond_with List.destroy(params[:id])
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end
