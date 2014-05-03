4d5fbac97865d42c370c70aea24a3f7a9ff81848
Maciej Hitsu Kowalski <maciej33kowalski@gmail.com>
2014-05-03 15:36:40 +0200

    move some logic into Ember Views

--- app/assets/javascripts/views/lists_view.js
+++ app/assets/javascripts/views/lists_view.js
@@ -1,5 +1,10 @@
 // for more details see: http://emberjs.com/guides/views/
 
 EmTasks.ListsView = Ember.View.extend({
-  templateName: 'lists'
+  templateName: 'lists',
+
+  submit: function() {
+    this.get('controller').send('addList');
+    return false;
+  }
 });
