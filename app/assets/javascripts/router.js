3d857b3bd09f5ebaae9e16af315feba1125b6c26
Maciej Hitsu Kowalski <maciej33kowalski@gmail.com>
2014-05-03 12:20:57 +0200

    use JS for router

---
+++ app/assets/javascripts/router.js
@@ -0,0 +1,14 @@
+// For more information see: http://emberjs.com/guides/routing/
+
+// EmTasks.Router.map(function(){
+//   return this.route("lists", {
+//     path: '/'
+//   });
+// });
+
+// EmTasks.ListsRoute = Ember.Route.extend({
+//   setupController: function(controller) {
+//     return controller.set("content", []);
+//   }
+// });
+
