331969bf9751cc84cce195ed93cd2b66fa72dc90
Maciej Hitsu Kowalski <maciej33kowalski@gmail.com>
2014-05-03 17:40:44 +0200

    add way to destroy List

--- app/assets/javascripts/controllers/lists_controller.js
+++ app/assets/javascripts/controllers/lists_controller.js
@@ -4,5 +4,11 @@ EmTasks.ListsController = Em.ArrayController.extend({
       name: this.get('newListName')
     }).save();
     return this.set('newListName', '');
+  },
+
+  destroyList: function(id) {
+    this.get('store').find('list', id).then( function(record) {
+      record.destroyRecord();
+    });
   }
 });
