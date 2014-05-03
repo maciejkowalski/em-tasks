fb85fc23f036ab0ee04707e1d4a8c9f561d339a6
Maciej Hitsu Kowalski <maciej33kowalski@gmail.com>
2014-05-03 15:35:16 +0200

    commit save for List#create in Ember

--- app/assets/javascripts/controllers/lists_controller.js
+++ app/assets/javascripts/controllers/lists_controller.js
@@ -2,7 +2,7 @@ EmTasks.ListsController = Em.ArrayController.extend({
   addList: function() {
     this.store.createRecord('list', {
       name: this.get('newListName')
-    });
+    }).save();
     return this.set('newListName', '');
   }
 });
