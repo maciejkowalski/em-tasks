fa62c5fd337dc8f968e95f67956d54fb8e9f505b
Maciej Hitsu Kowalski <maciej33kowalski@gmail.com>
2014-05-05 22:18:33 +0200

    add editList action

--- app/assets/javascripts/controllers/lists_controller.js
+++ app/assets/javascripts/controllers/lists_controller.js
@@ -14,5 +14,24 @@ EmTasks.ListsController = Em.ArrayController.extend({
 });
 
 EmTasks.ListController = Em.ObjectController.extend({
-  isEditing: true
+  actions: {
+    editList: function() {
+      this.set('isEditing', true);
+    },
+    acceptChanges: function () {
+      this.set('isEditing', false);
+
+      if (Ember.isEmpty(this.get('model.name'))) {
+        this.send('removeList');
+      } else {
+        console.log('get model', this.get('model'));
+        this.get('model').save();
+      }
+    },
+    removeList: function () {
+      var list = this.get('model');
+      list.destroyRecord();
+    }
+  },
+  isEditing: false
 })
