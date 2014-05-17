33d00c8295f9fc62cfd903b53469851d95c6dfa0
Maciej Hitsu Kowalski <maciej33kowalski@gmail.com>
2014-05-17 18:09:15 +0200

    assign parameters correctly for user in profile-ctrl#init

--- app/assets/javascripts/controllers/profile_controller.js
+++ app/assets/javascripts/controllers/profile_controller.js
@@ -6,7 +6,14 @@ EmTasks.ProfileController = Em.ObjectController.extend({
     _this = this;
 
     Ember.$.getJSON('/users/user').then( function(response) {
-      var model = _this.store.createRecord('user', response.user);
+      var user = response.user;
+      var model = _this.store.createRecord('user', {
+        email: user.email,
+        avatar: user.avatar,
+        firstName: user.first_name,
+        lastName: user.last_name,
+        id: user.id,
+      });
       _this.set('model', model);
     });
   }
