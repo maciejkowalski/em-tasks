27fbc9454a751ebcc6db2316217537c606fc3b93
Maciej Hitsu Kowalski <maciej33kowalski@gmail.com>
2014-05-17 17:49:16 +0200

    camel case user.js attrs

--- app/assets/javascripts/models/user.js
+++ app/assets/javascripts/models/user.js
@@ -3,9 +3,9 @@
 EmTasks.User = DS.Model.extend({
   email: DS.attr('string'),
   password: DS.attr("string"),
-  password_confirmation: DS.attr("string"),
+  passwordConfirmation: DS.attr("string"),
   avatar: DS.attr('list'),
-  first_name: DS.attr('list'),
-  last_name: DS.attr('list'),
+  firstName: DS.attr('list'),
+  lastName: DS.attr('list'),
 });
 
