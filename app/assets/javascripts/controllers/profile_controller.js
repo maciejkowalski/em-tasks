5d893aae8c5029d6ca68644bfc6ef3d818e25afd
Maciej Hitsu Kowalski <maciej33kowalski@gmail.com>
2014-05-17 18:35:49 +0200

    tricky way to get currentUser in Ember =)

--- app/assets/javascripts/controllers/profile_controller.js
+++ app/assets/javascripts/controllers/profile_controller.js
@@ -6,15 +6,9 @@ EmTasks.ProfileController = Em.ObjectController.extend({
     _this = this;
 
     Ember.$.getJSON('/users/user').then( function(response) {
-      var user = response.user;
-      var model = _this.store.createRecord('user', {
-        email: user.email,
-        avatar: user.avatar,
-        firstName: user.first_name,
-        lastName: user.last_name,
-        id: user.id,
+      _this.store.find('user', response.user.id).then( function(record) {
+        _this.set('model', record);
       });
-      _this.set('model', model);
     });
   }
 });
