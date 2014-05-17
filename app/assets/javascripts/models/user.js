8b4acecf8d89b26f281b12943534324b7ce33b69
Maciej Hitsu Kowalski <maciej33kowalski@gmail.com>
2014-05-17 15:31:42 +0200

    fix user.js Em model

--- app/assets/javascripts/models/user.js
+++ app/assets/javascripts/models/user.js
@@ -4,8 +4,8 @@ EmTasks.User = DS.Model.extend({
   email: DS.attr('string'),
   password: DS.attr("string"),
   password_confirmation: DS.attr("string"),
-  avatar: DS.belongsTo('list'),
-  first_name: DS.belongsTo('list'),
-  last_name: DS.belongsTo('list'),
+  avatar: DS.attr('list'),
+  first_name: DS.attr('list'),
+  last_name: DS.attr('list'),
 });
 
