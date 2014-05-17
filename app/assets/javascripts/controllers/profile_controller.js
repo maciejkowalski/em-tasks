977ab6d2d2fe3c24fd8bcfd273d256b84423bbdf
Maciej Hitsu Kowalski <maciej33kowalski@gmail.com>
2014-05-17 15:35:44 +0200

    implement ProfileController with non-rest URI for model

--- app/assets/javascripts/controllers/profile_controller.js
+++ app/assets/javascripts/controllers/profile_controller.js
@@ -1,3 +1,13 @@
-EmTasks.ProfileController = Em.ArrayController.extend({
+EmTasks.ProfileController = Em.ObjectController.extend({
+  content: null,
+  currentUser: null,
 
-})
+  init: function() {
+    _this = this;
+
+    Ember.$.getJSON('/users/user').then( function(response) {
+      var model = _this.store.createRecord('user', response.user);
+      _this.set('model', model);
+    });
+  }
+});
