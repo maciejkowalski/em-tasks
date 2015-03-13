a7d3c06f663689823e04f3e7dc2dfbeee42f1378
Maciej Kowalski <maciej33kowalski@gmail.com>
2015-03-13 14:47:05 +0100

    add code for profileRoute , initialize navbar view

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -16,4 +16,22 @@ EmTasks.ListsRoute = Ember.Route.extend({
   }
 });
 
-EmTasks.ProfileRoute = Em.Route.extend({});
+EmTasks.ProfileRoute = Em.Route.extend({
+  model: function() {
+    self = this;
+    Ember.$.getJSON('/users/user').then( function(response) {
+      self.store.find('user', response.user.id).then( function(record) {
+        return record;
+        // self.set('model', record);
+      });
+    });
+  },
+
+  setupController: function(controller, model) {
+    var navbarView = EmTasks.NavbarView.create();
+    console.log('view', navbarView);
+    navbarView.replaceIn("#navbar_user");
+    this._super
+
+  },
+});
