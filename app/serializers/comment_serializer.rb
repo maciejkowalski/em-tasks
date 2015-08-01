c80f1751d0514d74fd3ca3b7c74e202d2970cb6d
Maciej Kowalski <maciej33kowalski@gmail.com>
2015-08-01 22:43:12 +0200

    extend Comment serializer with user data

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,5 +1,22 @@
 class CommentSerializer < ActiveModel::Serializer
   embed :ids, include: true
 
-  attributes :id, :content, :task_id, :user_id, :created_at
+  attributes :id, :content, :task_id, :user_id, :created_at,
+             :user_name, :user_email, :user_avatar
+
+  def created_at
+    object.created_at.strftime("%Y/%m/%d %H:%M")
+  end
+
+  def user_email
+    object.user.email
+  end
+
+  def user_name
+    "#{object.user.first_name} #{object.user.last_name}"
+  end
+
+  def user_avatar
+    object.user.avatar
+  end
 end
