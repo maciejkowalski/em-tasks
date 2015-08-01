04b57ab977fb43adb3bb7274c976010fb2673209
Maciej Kowalski <maciej33kowalski@gmail.com>
2015-08-01 19:06:30 +0200

    add autoexpanding edit text area view and helper

--- app/assets/javascripts/views/edit_input_view.js
+++ app/assets/javascripts/views/edit_input_view.js
@@ -3,5 +3,28 @@ EmTasks.EditInputView = Ember.TextField.extend({
     this.$().focus();
   }
 });
-
 Ember.Handlebars.helper('edit-input', EmTasks.EditInputView);
+
+
+// http://ember.zone/a-simple-auto-expanding-textarea-component/
+EmTasks.EditTextAutoExpandingView = Ember.TextArea.extend({
+    didInsertElement: function(){
+        Ember.run.next(function() {
+            this.$().focus();
+            this.$().on('keypress', function(e) {
+                var textArea = $(this);
+                var newHeight = this.scrollHeight + parseFloat(textArea.css("borderTopWidth")) +
+                                parseFloat(textArea.css("borderBottomWidth"));
+
+                while(textArea.outerHeight() < newHeight) {
+                    $(this).height($(this).height() + 1);
+                }
+            });
+        }.bind(this));
+    },
+
+    willDestroyElement: function() {
+        this.$().off('keypress');
+    }
+});
+Ember.Handlebars.helper('edit-text', EmTasks.EditTextAutoExpandingView);
