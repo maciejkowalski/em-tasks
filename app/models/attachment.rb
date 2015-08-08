299e1e5903676b4ee42258206a3af7058310af21
Maciej Kowalski <maciej33kowalski@gmail.com>
2015-08-08 14:38:00 +0200

    mount carrierwave uploader in attachment model, add some helpers

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,4 +1,14 @@
 class Attachment < ActiveRecord::Base
   belongs_to :task
   belongs_to :user
+
+  mount_uploader :attachment, ::AttachmentUploader
+
+  def file_name
+    attachment.try(:file).try(:filename)
+  end
+
+  def url
+    attachment.try(:url)
+  end
 end
